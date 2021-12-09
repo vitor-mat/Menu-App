ed269eb01e37c9f26015be8eab54df6be9146325
vitor-mat <vitormateusalves123@gmail.com>
2021-12-09 08:06:30 -0300

    quando voê clica na logo ele volta a pagina inicial

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,29 @@ import React from 'react';
 
 import { motion } from 'framer-motion';
 
+import { Link } from 'react-router-dom';
+
 const svgVariants = {
-  hidden:{
+  hidden: {
     rotate: -180
   },
-  visible:{
+  visible: {
     rotate: 0,
-    transition:{
+    transition: {
       duration: 1
     }
   }
 }
 
 const pathVariants = {
-  hidden:{
+  hidden: {
     opacity: 0,
     pathLength: 0
   },
-  visible:{
+  visible: {
     opacity: 1,
     pathLength: 1,
-    transition:{
+    transition: {
       duration: 2,
       ease: "easeInOut"
     }
@@ -34,32 +36,36 @@ const Header = () => {
     <header>
       <motion.div className="logo"
         drag
-        dragConstraints={{top: 0, right: 0, bottom: 50, left: 0}}
+        dragConstraints={{ top: 0, right: 0, bottom: 50, left: 0 }}
         dragElastic={2}
       >
-        <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
-          variants={svgVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <motion.path
-            fill="none"
-            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-            variants={pathVariants}
-          />
-          <motion.path
-            fill="none"
-            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-            variants={pathVariants}
-          />
-        </motion.svg>
+        <Link to="/home">
+          <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+            variants={svgVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            <motion.path
+              fill="none"
+              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+              variants={pathVariants}
+            />
+            <motion.path
+              fill="none"
+              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+              variants={pathVariants}
+            />
+          </motion.svg>
+        </Link>
       </motion.div>
       <motion.div className="title"
-        animate={{y:-10}}
-        initial={{y:-250}}
-        transition={{delay:0.2, type:"spring", stiffness:120}}
+        animate={{ y: -10 }}
+        initial={{ y: -250 }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
       >
-        <h1>Menu App</h1>
+        <Link to="/home">
+          <h1>Menu App</h1>
+        </Link>
       </motion.div>
     </header>
   )
