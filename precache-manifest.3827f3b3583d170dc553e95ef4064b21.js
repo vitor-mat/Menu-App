c7f87b24fd71043ec502d1468516976a494d0028
vitor-mat <vitormateusalves123@gmail.com>
2021-12-09 18:19:02 -0300

    Updates

---
+++ precache-manifest.3827f3b3583d170dc553e95ef4064b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42203edf256e2d24a00f3143ac240287",
+    "url": "/Menu-App/index.html"
+  },
+  {
+    "revision": "4b65104dea17eb129f0d",
+    "url": "/Menu-App/static/css/main.35d9ac77.chunk.css"
+  },
+  {
+    "revision": "0d0924eaffa72f705f06",
+    "url": "/Menu-App/static/js/2.65e48602.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Menu-App/static/js/2.65e48602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b65104dea17eb129f0d",
+    "url": "/Menu-App/static/js/main.a7564ee2.chunk.js"
+  },
+  {
+    "revision": "6908626b21746009bfd4",
+    "url": "/Menu-App/static/js/runtime-main.f5ef5a62.js"
+  }
+]);
